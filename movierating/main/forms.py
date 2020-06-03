7f5d3de18d6debc1d1189ca950198e93b210de8a
yonasyiheyis <49254355+yonasyiheyis@users.noreply.github.com>
2020-06-03 22:21:28 +0300

    First commit

--- movierating/main/forms.py
+++ movierating/main/forms.py
@@ -5,4 +5,4 @@ from .models import *
 class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
-        fields = ('name', 'director', 'cast', 'description', 'release_date', 'image')
+        fields = ('name', 'director', 'cast', 'description', 'release_date', 'averageRating', 'image')
